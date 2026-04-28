2bd3735bb167ea4f4f2db351981abb09a9716fb7
Lê Công Vinh <25023546@vnu.edu.vn>
2026-04-28 11:29:23 +0700

    second

--- LeCongVinh_25023546_Week9/Bai06/src/main/java/App.java
+++ LeCongVinh_25023546_Week9/Bai06/src/main/java/App.java
@@ -1,6 +1,32 @@
 public class App {
-
+//
     public String sayHello() {
         return "Hello CI/CD";
     }
 }
+//Lần 1:
+//Maven sẽ tải dependency từ đầu
+//→ chạy chậm hơn
+//→ log sẽ có download từ Maven Central
+
+// lần 2
+//Maven dùng cache
+//→ chạy nhanh hơn
+//→ log sẽ hiện restored cache
+//→ không tải lại dependency
+
+//First workflow run:
+//Duration: 2 min 10 sec
+//
+//Second workflow run:
+//Duration: 45 sec
+
+//Sau khi cấu hình cache: maven trong setup-java,
+//thời gian thực thi workflow giảm đáng kể.
+//
+//Lần chạy đầu Maven phải tải toàn bộ dependency
+//từ Maven Central nên mất nhiều thời gian hơn.
+//
+//Từ lần chạy sau, GitHub Actions sử dụng cache
+//để khôi phục dependency đã tải trước đó,
+//giúp build nhanh hơn và tối ưu pipeline CI/CD.
